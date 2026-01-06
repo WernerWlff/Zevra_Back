738e5863691f5242077053933262cfb0556cbb5a
Werner Wolff <werner.wolff1109@gmail.com>
2026-01-06 14:23:21 +0100

    add:initial setup to the project
    add:
    - springboot starter data
    - springboot starter security
    - springboot starter web
    - springboot starter validation
    - postgresql
    - flywaydb
    
    add: corsConfig to connect to the frontend
    
    need: application properties and the config into a .env file

---
+++ zevra/src/main/java/com/zevra/zevra/ZevraApplication.java
@@ -0,0 +1,13 @@
+package com.zevra.zevra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZevraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZevraApplication.class, args);
+	}
+
+}
---
+++ zevra/src/test/java/com/zevra/zevra/ZevraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zevra.zevra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZevraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
