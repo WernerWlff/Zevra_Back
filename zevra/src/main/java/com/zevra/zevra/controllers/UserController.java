3b5ccf527fe468208002e8f16a685cd515d248e4
Werner Wolff <werner.wolff1109@gmail.com>
2026-01-13 14:26:51 +0100

    add: CRUD to the user Table

--- zevra/src/main/java/com/zevra/zevra/controllers/UserController.java
+++ zevra/src/main/java/com/zevra/zevra/controllers/UserController.java
@@ -1,4 +1,63 @@
 package com.zevra.zevra.controllers;
 
+import com.zevra.zevra.entities.User;
+import com.zevra.zevra.services.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/user")
 public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
+    }
+
+    @GetMapping("/id")
+    public ResponseEntity<User> getUserById(@PathVariable UUID id) {
+        Optional<User> user = userService.getUserById(id);
+
+        if(user.isPresent()){
+            return new ResponseEntity<>(user.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping(consumes = "application/json", produces = "application/json")
+    public ResponseEntity<User> updateUser(@RequestBody User user) {
+        User userCreated = userService.createUser(user);
+        return new ResponseEntity<>(userCreated, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/id")
+    public ResponseEntity<User> updateUserById(@PathVariable UUID id, @RequestBody User userDetails) {
+        Optional<User> user = userService.updateUser(id, userDetails);
+
+        if(user.isPresent()){
+            return new ResponseEntity<>(user.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/id")
+    public ResponseEntity<Void> deleteUser(@PathVariable UUID id) {
+        Optional<User> deletedUser = userService.deleteUser(id);
+
+        if(deletedUser.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
 }
--- zevra/src/main/java/com/zevra/zevra/services/UserService.java
+++ zevra/src/main/java/com/zevra/zevra/services/UserService.java
@@ -1,4 +1,58 @@
 package com.zevra.zevra.services;
 
+import com.zevra.zevra.entities.User;
+import com.zevra.zevra.repositories.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
 public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(UUID id) {
+        return userRepository.findById(id);
+    }
+
+    @Transactional
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Transactional
+    public Optional<User>updateUser(UUID id, User user) {
+        Optional<User> optionalUser = userRepository.findById(id);
+
+        if(optionalUser.isPresent()) {
+            User existingUser = optionalUser.get();
+            existingUser.setFirstname(user.getFirstname());
+            existingUser.setLastname(user.getLastname());
+            existingUser.setUsername(user.getUsername());
+            existingUser.setEmail(user.getEmail());
+            existingUser.setPassword(user.getPassword());
+            User savedUser = userRepository.save(existingUser);
+            return Optional.of(savedUser);
+        }
+        return Optional.empty();
+    }
+
+    @Transactional
+    public Optional<User>deleteUser(UUID id) {
+        Optional<User> user = userRepository.findById(id);
+        if(user.isPresent()) {
+            userRepository.deleteById(id);
+        }
+        return user;
+    }
 }
