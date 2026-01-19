2676873d9b494bb37f20881e6436838196a06392
Werner Wolff <werner.wolff1109@gmail.com>
2026-01-19 10:56:24 +0100

    chore: changed the localhost port for CorsConfig and SecurityConfig in the .env file.
    feat : added in .env.example what to put in

--- zevra/src/main/java/com/zevra/zevra/config/CorsConfig.java
+++ zevra/src/main/java/com/zevra/zevra/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("${local_port}")
                 .allowedMethods("GET","POST","PUT","DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
